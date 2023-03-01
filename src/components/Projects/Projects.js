45c6c53493f13965125507a50343ea69508f7844
Andrew Milner <95140821+milnera92@users.noreply.github.com>
2023-03-01 10:31:45 -0500

    added merchant viewer project and new resume

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,6 +6,7 @@ import Particle from "../Particle";
 import encore from "../../Assets/Projects/encore.png";
 import token from "../../Assets/Projects/token.png";
 import nordtech from "../../Assets/Projects/nordtech.png";
+import merchant from "../../Assets/Projects/merchant.png";
 
 function Projects() {
   return (
@@ -55,15 +56,23 @@ function Projects() {
             />
           </Col>
 
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={merchant}
+              isBlog={false}
+              title="Merchant Portal"
+              description="Transaction viewer for Merchants. Built in JS, PHP and MySQL. Hosted on AWS and Heroku/Render"
+              ghLink="https://github.com/milnera92/merchant-viewer-fe"
+              demoLink="https://merchant-viewer.onrender.com/"
+            />
+          </Col>
+
           {/* <h1 className="project-heading">
           Some <strong className="blue">Graphic Design </strong>
         </h1>
         <p style={{ color: "white" }}>
           *Please note this work is close to 10 years old. Images are low quality due to being pulled from a backup.
         </p> */}
-
-
-
         </Row>
       </Container>
     </Container>
